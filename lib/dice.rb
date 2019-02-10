9ce4ab994f44d36f29047adcbe0efc91bfa764fe
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-10 18:51:42 +0000

    Provide dice throw values for a specified number of dice

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,12 @@
 class Dice
-  def roll
-    rand(6) + 1
+  DEFAULT_NUMBER_OF_DICE = 1
+
+  def initialize
+    @dice_throw_values = []
+  end
+  def roll(number_of_dice = DEFAULT_NUMBER_OF_DICE)
+    @number_of_dice = number_of_dice
+    number_of_dice.times
+      @dice_throw_values << rand(6) + 1
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,11 +1,17 @@
 require 'dice.rb'
 describe Dice do
   it 'provides a number when rolled' do
-    expect(subject.roll).to be_a Integer
+    subject.roll.each { |value| expect(value).to be_a Integer }
   end
 
   it 'provides a random number between 1 and 6 when rolled' do
-    expect(subject.roll).to be > 0
-    expect(subject.roll).to be < 7
+    subject.roll.each { |value| expect(value).to be_between(1,6).inclusive }
   end
+
+  it 'provides a specified set of random numbers between 1 and 6 when asked to roll multiple number of dice' do
+    allowed_values = [1, 2, 3, 4, 5, 6]
+    dicethrows = subject.roll(2)
+    dicethrows.each { |value| expect(allowed_values).to include(value) }
+  end
+
 end
