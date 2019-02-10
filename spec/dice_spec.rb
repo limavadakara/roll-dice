a548d000fc0794e1d58b0f19a0c52c502d6e1866
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-10 18:07:15 +0000

    provide a random number between 1 and 6

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,4 +3,9 @@ describe Dice do
   it 'provides a number when rolled' do
     expect(subject.roll).to be_a Integer
   end
+
+  it 'provides a random number between 1 and 6 when rolled' do
+    expect(subject.roll).to be > 0
+    expect(subject.roll).to be < 7
+  end
 end
